7c6683409f1d303f6849fde65e11d0ced455cde8
Jiseok CHOI <jiseok@hyperithm.com>
2020-11-08 16:29:44 +0900

    programmers H-index solve(cpp)

---
+++ programmers/H-index/main.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(int a, int b)
+{
+    return a > b;
+}
+
+int solution(vector<int> citations)
+{
+    int answer = 0;
+    sort(citations.begin(), citations.end(), compare);
+
+    if (citations[0] == 0)
+    {
+        return 0;
+    }
+
+    for (int i = 0; i < citations.size(); i++)
+    {
+        if (citations[i] > i)
+            answer++;
+        else
+            break;
+    }
+
+    return answer;
+}
