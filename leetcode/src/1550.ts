97366d1020d59341b5d3fa344f25b447e2746128
Jiseok CHOI <jiseok.dev@gmail.com>
2022-07-07 19:08:02 +0900

    [leetcode](typescript) Remove unnecessary if guard

--- leetcode/src/1550.ts
+++ leetcode/src/1550.ts
@@ -4,10 +4,6 @@ export {};
  * Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
  */
 function threeConsecutiveOdds(arr: number[]): boolean {
-    if (arr.length < 3) {
-        return false;
-    }
-
     for (let i = 0; i <= arr.length - 3; i++) {
         if (arr[i] % 2 === 1 && arr[i + 1] % 2 === 1 && arr[i + 2] % 2 === 1) {
             return true;
